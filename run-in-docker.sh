10a927405142f1e8a013f3328d05fc0b84e83992
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-21 07:38:37 +0200

    made solution folder arg to have trailing slash

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -8,17 +8,17 @@
 
 # Arguments:
 # $1: exercise slug
-# $2: **RELATIVE** path to solution folder (without trailing slash)
+# $2: **RELATIVE** path to solution folder (with trailing slash)
 
 # Output:
 # [For now] writes the tests output to the terminal
 
 # Example:
-# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder
+# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder/
 
 # If arguments not provided, print usage and exit
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "usage: run-in-docker.sh exercise-slug ./relative/path/to/solution/folder"
+    echo "usage: run-in-docker.sh exercise-slug ./relative/path/to/solution/folder/"
     exit 1
 fi
 
@@ -26,4 +26,4 @@ fi
 docker build -t python-test-runner .
 
 # run image passing the arguments
-docker run -v $PWD/$2:/solution python-test-runner $1 /solution
+docker run -v $PWD/$2:/solution python-test-runner $1 /solution/
