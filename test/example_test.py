fcdc3891a8ce0bcb4924d84e18830022bc36b374
Corey McCandless <corey.mccandless@atlasied.com>
2019-09-23 10:32:05 -0400

    Sort test results in results.json by test file order

--- test/example_test.py
+++ test/example_test.py
@@ -9,6 +9,9 @@ class ExampleTest(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello(), 'Hello, World!')
 
+    def test_abc(self):
+        self.assertIs(True, True)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ verify_test_order.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+EXERCISE_SLUG="$1"
+SOLUTION_DIR="$2"
+OUTPUT_DIR="$3"
+
+TEST_FILE="$2/${1/-/_}_test.py"
+RESULTS_FILE="$3/results.json"
+
+from_test_file()
+{
+    grep -oP '(?<=def )(test_[^(]+)' "$TEST_FILE"
+}
+
+from_results_file()
+{
+    PYTHON_CODE=$(cat <<EOF
+import json
+import sys
+with open(sys.argv[1]) as f:
+    data = json.load(f)
+for test in data['tests']:
+    print(test['name'])
+
+EOF
+)
+    python -c "$PYTHON_CODE" "$RESULTS_FILE"
+}
+# echo "$TEST_FILE"
+# # from_test_file
+diff <(from_test_file) <(from_results_file)
