40ac357357f0ad6cd49ead988156dc7a6ed16cd7
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-10-20 09:39:41 +0200

    takes arguments with trailing slash

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -8,18 +8,18 @@
 
 # Arguments:
 # $1: exercise slug
-# $2: **RELATIVE** path to solution folder (without trailing slash)
-# $3: **RELATIVE** path to output directory (without trailing slash)
+# $2: **RELATIVE** path to solution folder (with trailing slash)
+# $3: **RELATIVE** path to output directory (with trailing slash)
 
 # Output:
 # [For now] writes the tests output to the terminal
 
 # Example:
-# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder ./relative/path/to/output/directory
+# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder/ ./relative/path/to/output/directory/
 
 # If arguments not provided, print usage and exit
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "usage: run-in-docker.sh exercise-slug ./relative/path/to/solution/folder ./relative/path/to/output/directory"
+    echo "usage: run-in-docker.sh exercise-slug ./relative/path/to/solution/folder/ ./relative/path/to/output/directory/"
     exit 1
 fi
 
@@ -30,4 +30,4 @@ docker build -t python-test-runner .
 docker run \
     --mount type=bind,src=$PWD/$2,dst=/solution \
     --mount type=bind,src=$PWD/$3,dst=/output \
-    python-test-runner $1 /solution /output
+    python-test-runner $1 /solution/ /output/
