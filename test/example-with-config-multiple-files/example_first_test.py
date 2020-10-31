c238da8938071ea3a40c9bf9b79818926ce7aa15
Corey McCandless <cmccandless@users.noreply.github.com>
2020-10-31 17:16:58 +0000

    fix tests

--- test/example-with-config-multiple-files/example_first_test.py
+++ test/example-with-config-multiple-files/example_first_test.py
@@ -5,14 +5,14 @@ from example_with_config import hello
 
 
 class ExampleFirstTest(unittest.TestCase):
-    def test_hello_again(self):
+    def test_hello(self):
         self.assertEqual(hello(), "Hello, World!")
 
     def test_abc(self):
         self.assertEqual(hello(), "Hello, World!")
 
 
-class ExampleFirstTest(unittest.TestCase):
+class ExampleFirstOtherTest(unittest.TestCase):
     def test_dummy(self):
         self.assertEqual(hello(), "Hello, World!")
 
