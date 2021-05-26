2c1f7fd47036628cb90e255f1dd2b77f67e1c142
BethanyG <bethanymgarcia@gmail.com>
2021-05-25 19:18:20 -0700

    Added golden file and tests for stdout with flagged exercises tasks.

---
+++ test/example-has-stdout-and-tasks/example_has_stdout_and_tasks_test.py
@@ -0,0 +1,35 @@
+import unittest
+import pytest
+
+
+from example_has_stdout_and_tasks import hello, must_truncate
+
+
+class ExampleHasStdoutTest(unittest.TestCase):
+
+    @pytest.mark.task(taskno=1)
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+    @pytest.mark.task(taskno=2)
+    def test_abc(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+    @pytest.mark.task(taskno=3)
+    def test_trancation(self):
+        self.assertEqual(must_truncate(), "Hello, World!")
+
+
+class ExampleHasStdoutOtherTest(unittest.TestCase):
+
+    @pytest.mark.task(taskno=4)
+    def test_dummy(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+    @pytest.mark.task(taskno=5)
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
