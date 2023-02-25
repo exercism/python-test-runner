020aca1b32a78825f459d6e912047852ca172cff
BethanyG <BethanyG@users.noreply.github.com>
2023-02-24 18:42:21 -0800

    Added new goldenfile tests for lasagna import errors.

---
+++ test/example-lasagna-function-import-error/example_lasagna_function_import_error.py
@@ -0,0 +1,23 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
+"""
+
+
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+
+
+# TODO: define the 'bake_time_remaining()' function (misspelled here on purpose)
+def bake_time():
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    pass
