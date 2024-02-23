a734e3f35fdde6e07e39fce9f4d3be3ac20e8830
Ashish Keshan <ashishkeshan@github.com>
2024-02-23 15:31:54 -0500

    add more prints

--- runner/__init__.py
+++ runner/__init__.py
@@ -198,6 +198,7 @@ def run(indir: Directory, outdir: Directory, args: List[str]) -> None:
 
     print(indir)
     for root, dirs, files in os.walk(indir):
+        print(root, dirs, files)
         for file in files:
             if file.endswith("_test.py"):
                 test_files.append(Path(root) / file)
