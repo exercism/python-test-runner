9ddc30191ec52ea32ad4279bc129028e4bac4c32
Ashish Keshan <ashishkeshan@github.com>
2024-02-29 12:02:24 -0500

    add print statement

--- bin/run.sh
+++ bin/run.sh
@@ -7,6 +7,8 @@ mkdir autograding_output
 
 MAX_SCORE="$1"
 
+echo "max score is $MAX_SCORE"
+
 python3 /opt/test-runner/bin/run.py ./ ./autograding_output/ "$MAX_SCORE"
 
 echo "result=$(jq -c . autograding_output/results.json | jq -sRr @base64)" >> "$GITHUB_OUTPUT"
